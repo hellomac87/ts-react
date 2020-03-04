6d9bd0e65a16e06835ba42638ec1c95621b53d08
hellomac87 <hellomac87@gmail.com>
2020-03-04 22:13:18 +0900

    2-3. setTimeout, useRef 타이핑

--- 3./client.tsx
+++ 3./client.tsx
@@ -2,8 +2,8 @@ import * as React from "react";
 import * as ReactDOM from "react-dom";
 import { hot } from "react-hot-loader/root";
 
-import ResponseCheck from "./ResponseCheck";
+import NumberBaseball from "./NumberBaseball";
 
-const Hot = hot(ResponseCheck);
+const Hot = hot(NumberBaseball);
 
 ReactDOM.render(<Hot />, document.querySelector("#root"));
