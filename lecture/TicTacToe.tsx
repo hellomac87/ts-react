4f69aa50218200b91d99d0586c0604f12a19fec5
hellomac87 <hellomac87@gmail.com>
2020-03-13 23:15:46 +0900

    4-2. Dispatch, children

--- lecture/TicTacToe.tsx
+++ lecture/TicTacToe.tsx
@@ -110,6 +110,64 @@ const TicTacToe = () => {
   const [state, dispatch] = useReducer(reducer, itnitialState);
   const { tableData, turn, winner, recentCell } = state;
 
+  useEffect(() => {
+    // 승자 가리는 effect
+    const [row, cell] = recentCell;
+    if (row < 0) {
+      return;
+    }
+    let win = false;
+    if (
+      tableData[row][0] === turn &&
+      tableData[row][1] === turn &&
+      tableData[row][2] === turn
+    ) {
+      win = true;
+    }
+    if (
+      tableData[0][cell] === turn &&
+      tableData[1][cell] === turn &&
+      tableData[2][cell] === turn
+    ) {
+      win = true;
+    }
+    if (
+      tableData[0][0] === turn &&
+      tableData[1][1] === turn &&
+      tableData[2][2] === turn
+    ) {
+      win = true;
+    }
+    if (
+      tableData[0][2] === turn &&
+      tableData[1][1] === turn &&
+      tableData[2][0] === turn
+    ) {
+      win = true;
+    }
+    console.log(win, row, cell, tableData, turn);
+    if (win) {
+      // 승리시
+      dispatch({ type: SET_WINNER, winner: turn });
+      dispatch({ type: RESET_GAME });
+    } else {
+      let all = true; // all이 true면 무승부라는 뜻
+      tableData.forEach(row => {
+        // 무승부 검사
+        row.forEach(cell => {
+          if (!cell) {
+            all = false;
+          }
+        });
+      });
+      if (all) {
+        dispatch({ type: RESET_GAME });
+      } else {
+        dispatch({ type: CHANGE_TURN });
+      }
+    }
+  }, [recentCell]);
+
   const onClickTable = useCallback(() => {
     dispatch(setWinner("O"));
   }, []);
