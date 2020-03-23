5af560ecbdf0c0d186cb610c3e6257272a19673a
hellomac87 <hellomac87@gmail.com>
2020-03-23 22:49:10 +0900

    7-4. redux-thunk 타이핑

--- lecture/actions/user.ts
+++ lecture/actions/user.ts
@@ -1,3 +1,6 @@
+import { AnyAction } from "redux";
+import { addPost } from "./post";
+
 export const LOG_IN_REQUEST = "LOG_IN_REQUEST" as const;
 export const LOG_IN_SUCCESS = "LOG_IN_SUCCESS" as const;
 export const LOG_IN_FAILURE = "LOG_IN_FAILURE" as const;
@@ -9,19 +12,67 @@ export interface LogInRequestAction {
   data: { id: string; password: string };
 }
 
+export const loginRequest = (data: {
+  id: string;
+  password: string;
+}): LogInRequestAction => {
+  return {
+    type: LOG_IN_REQUEST,
+    data
+  };
+};
+
 export interface LogInSuccessAction {
   type: typeof LOG_IN_SUCCESS;
-  data: { userId: string; nickname: string };
+  data: { userId: number; nickname: string };
 }
+
+export const loginSuccess = (data: {
+  userId: number;
+  nickname: string;
+}): LogInSuccessAction => {
+  return {
+    type: LOG_IN_SUCCESS,
+    data
+  };
+};
 
 export interface LogInFailureAction {
   type: typeof LOG_IN_FAILURE;
   error: Error;
 }
 
-export const logIn = (data: { id: string; password: string }) => {
+export const loginFailure = (error: Error): LogInFailureAction => {
   return {
-    type: LOG_IN_REQUEST
+    type: LOG_IN_FAILURE,
+    error
+  };
+};
+
+interface ThunkDispatch {
+  (thunkAction: ThunkAction): void;
+  <A>(action: A): A;
+  <TAction>(action: TAction | ThunkAction): TAction;
+}
+
+type ThunkAction = (dispatch: ThunkDispatch) => void;
+
+export const logIn = (data: { id: string; password: string }): ThunkAction => {
+  return dispatch => {
+    dispatch(loginRequest(data));
+    try {
+      setTimeout(() => {
+        dispatch(
+          loginSuccess({
+            userId: 1,
+            nickname: "dobby"
+          })
+        );
+        dispatch(addPost(""));
+      }, 1000);
+    } catch (error) {
+      dispatch(loginFailure(error));
+    }
   };
 };
 
--- lecture/store.ts
+++ lecture/store.ts
@@ -1,5 +1,12 @@
-import { createStore } from "redux";
-
+import {
+  createStore,
+  MiddlewareAPI,
+  Dispatch,
+  AnyAction,
+  compose,
+  applyMiddleware
+} from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
 import reducers from "./reducers";
 
 const initialState = {
@@ -10,6 +17,28 @@ const initialState = {
   posts: []
 };
 
-const store = createStore(reducers, initialState);
+const firstMiddleware = (store: MiddlewareAPI) => (
+  next: Dispatch<AnyAction>
+) => (action: AnyAction) => {
+  console.log("로깅", action);
+  next(action);
+};
+
+const thunkMiddleware = (store: MiddlewareAPI) => (
+  next: Dispatch<AnyAction>
+) => (action: any) => {
+  if (typeof action === "function") {
+    //비동기
+    return action(store.dispatch, store.getState);
+  }
+  return next(action); //동기
+};
+
+const enhancer =
+  process.env.NODE_ENV === "production"
+    ? compose(applyMiddleware(firstMiddleware))
+    : composeWithDevTools(applyMiddleware(firstMiddleware, thunkMiddleware));
+
+const store = createStore(reducers, initialState, enhancer);
 
 export default store;
